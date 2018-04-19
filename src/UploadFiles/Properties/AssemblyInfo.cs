48318b4b369a4a56e0e3602d2afe3e186275eb88
Aspallar <shadowthrone99@outlook.com>
2018-04-19 20:18:21 +0100

    Added UploadFiles (no implementation)

---
+++ src/UploadFiles/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UploadFiles")]
+[assembly: AssemblyDescription("Bulk file upload")]
+[assembly: AssemblyProduct("UploadFiles")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a5223520-a620-4098-8a65-fce5c7633cb8")]
