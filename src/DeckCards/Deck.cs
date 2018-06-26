e74d0f927f2b3ff6b959d41e4475294b5d41c90a
Aspallar <shadowthrone99@outlook.com>
2018-06-26 22:46:14 +0100

    deckcards

---
+++ src/DeckCards/Deck.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+namespace DeckCards
+{
+    internal class Deck
+    {
+        public string Title { get; private set; }
+        public List<string> Cards { get; private set; }
+
+        public Deck(string title, List<string> cards)
+        {
+            Title = title;
+            Cards = cards;
+        }
+    }
+}
---
+++ src/DeckCards/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace DeckCards
+{
+    class Program
+    {
+        static WebClient client;
+        const string baseUrl = "http://magicarena.wikia.com/";
+        const string apiUrl = baseUrl + "api.php";
+        const string deckUrl = baseUrl + "wiki/Decks/";
+
+        static void Main(string[] args)
+        {
+            using (client = new WebClient())
+            {
+                var cardNames = ReadCardNames();
+                Dictionary<string, List<string>> cards = CardsFromDecks(cardNames);
+                Console.Error.WriteLine(new string('=', 20));
+                WriteCards(cards);
+            }
+            //Console.ReadKey();
+        }
+
+        private static Dictionary<string, string> ReadCardNames()
+        {
+            string line;
+            var cardNames = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase);
+            using (var sr = new StreamReader("cardnames.txt"))
+            {
+                while ((line = sr.ReadLine()) != null)
+                    cardNames.Add(line, line.Trim());
+            }
+            return cardNames;
+        }
+
+        private static void WriteCards(Dictionary<string, List<string>> cards)
+        {
+            List<string> sorted = cards.Keys.ToList();
+            sorted.Sort();
+            foreach (var card in sorted)
+            {
+                Console.WriteLine($"'''{{{{Card|{card}}}}}'''");
+                foreach (var deck in cards[card])
+                    Console.WriteLine($"*[[{deck}|{deck.Substring(6)}]]");
+            }
+        }
+
+        private static Dictionary<string, List<string>> CardsFromDecks(Dictionary<string, string> cardNames)
+        {
+            var cards = new Dictionary<string, List<string>>();
+            foreach (var deck in GetDecks())
+            {
+                Console.Error.WriteLine($"{deck.Title} {deck.Cards.Count}");
+                foreach (var card in deck.Cards)
+                {
+                    string correctCase;
+                    if (cardNames.TryGetValue(card, out correctCase))
+                    {
+                        if (cards.ContainsKey(correctCase))
+                        {
+                            cards[correctCase].Add(deck.Title);
+                        }
+                        else
+                        {
+                            var decks = new List<string>();
+                            decks.Add(deck.Title);
+                            cards.Add(correctCase, decks);
+                        }
+                    }
+                }
+            }
+            return cards;
+        }
+
+        private static IEnumerable<Deck> GetDecks()
+        {
+            var apfrom = "";
+            var decks = new XmlDocument();
+            var rev = new XmlDocument();
+            var url = BuildApiUrl(new Dictionary<string, string>
+            {
+                { "list", "allpages" },
+                { "apprefix", "Decks/" },
+                { "aplimit", "500" },
+                { "apfrom", ""},
+            });
+            do
+            {
+                string deckxml = client.DownloadString(url + apfrom);
+                decks.LoadXml(deckxml);
+                var pages = decks.SelectNodes("/api/query/allpages/p");
+                var cont = decks.SelectSingleNode("/api/query-continue/allpages");
+                apfrom = cont == null ? "" : cont.Attributes["apfrom"].Value;
+                foreach (XmlNode node in pages)
+                {
+                    var revisionUrl = BuildApiUrl(new Dictionary<string, string>
+                    {
+                        { "prop", "revisions" },
+                        { "rvprop", "content" },
+                        { "rvlimit", "1" },
+                        { "pageids", node.Attributes["pageid"].Value },
+                    });
+                    string content = client.DownloadString(revisionUrl);
+                    rev.LoadXml(content);
+                    var revNode = rev.SelectSingleNode("/api/query/pages/page/revisions/rev");
+                    List<string> cards = GetCards(revNode.InnerText);
+                    yield return new Deck(node.Attributes["title"].Value, cards);
+                }
+            } while (!string.IsNullOrEmpty(apfrom));
+        }
+
+        private static List<string> GetCards(string pageText)
+        {
+            char[] newline = { '\n' };
+            var cards = new List<string>();
+            string cardText = ExtractCardText(pageText);
+            foreach (string cardLine in cardText.Split(newline))
+            {
+                string trimmed = cardLine.Trim();
+                if (trimmed.Length > 0) {
+                    if (trimmed.StartsWith("--"))
+                        break; // reached sideboard, ignore the rest
+                    Match match = Regex.Match(trimmed, @"\d+\s+([^\(/]+)");
+                    if (match.Success)
+                    {
+                        string name = match.Groups[1].Value.Trim();
+                        cards.Add(name);
+                    }
+                }
+            }
+            return cards;
+        }
+
+        private static string ExtractCardText(string content)
+        {
+            int startPos = content.IndexOf("|Deck=", StringComparison.InvariantCultureIgnoreCase);
+            if (startPos == -1)
+                return string.Empty;
+            startPos += 6;
+            int endPos = content.IndexOf("}}", startPos);
+            if (endPos == -1)
+                return string.Empty;
+            string cardText = content.Substring(startPos, endPos - startPos);
+            return cardText;
+        }
+
+        private static string BuildApiUrl(Dictionary <string, string> queryParameters)
+        {
+            var url = new StringBuilder(apiUrl);
+            url.Append("?action=query&format=xml");
+            foreach (var entry in queryParameters)
+            {
+                url.Append('&');
+                url.Append(entry.Key);
+                url.Append('=');
+                url.Append(entry.Value);
+            }
+            return url.ToString(); ;
+        }
+
+    }
+}
