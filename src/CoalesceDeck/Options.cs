17753c319f7ad6eb292a0bb329c3f1ce387df4f0
Aspallar <shadowthrone99@outlook.com>
2019-11-24 21:50:23 +0000

    CoalesceDeck started (wip)

---
+++ src/CoalesceDeck/Options.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace CoalesceDeck
+{
+    class Options
+    {
+        [Value(0, MetaName = "Deck Title")]
+        public string Title { get; set; }
+
+        [Option]
+        public string User { get; set; }
+
+        [Option]
+        public string Password { get; set; }
+    }
+}
